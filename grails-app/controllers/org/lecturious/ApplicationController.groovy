89bd6d538d21c47faf75962bf49e2fb535fd9e64
Florian Motlik <flomotlik@gmail.com>
2009-10-22 16:14:18 +0200

    Application controller saving development User to database

--- grails-app/controllers/org/lecturious/ApplicationController.groovy
+++ grails-app/controllers/org/lecturious/ApplicationController.groovy
@@ -2,52 +2,53 @@ package org.lecturious
 
 class ApplicationController {
 
-    def persistenceManager
+  def persistenceManager
 
-    def facebookService
+  def facebookService
 
-    def index = {
-        if(session.user == null){
-            try{
-                switch (grails.util.GrailsUtil.environment){
-                    case "development":
-                    session.user = new User(name:"Name", facebookId:"12345")
-                    break
-
-                    case "production":
-                    def facebook = facebookService.getFacebookConnection(request, response)
-                    
-                    def facebookUserId = facebook.users_getLoggedInUser();
-                    def username = facebook.users_getInfo([facebookUserId], ["name"]).get(0).name
-                    
-                    def query = persistenceManager.newQuery(User.class);
-                    query.setFilter("facebookId == facebookIdParam");
-                    query.declareParameters("String facebookIdParam");
-
-                    def user = query.execute(facebookUserId.toString())
-                    if(!user){
-                        session.user = new User(facebookId:facebookUserId, name:username)
-                        log.debug("Persisting user ${session.user}")
-                        persistenceManager.makePersistent(session.user)
-                    }else{
-                        session.user = new User(facebookId:user.facebookId, name:user.name, id:user.id)
-                    }
-                    
-                    log.debug("User: ${session.user.name}")
-                    break
-                }
-            }catch(e){
-                log.debug(e)
+  def index = {
+    if (session.user == null) {
+      try {
+        switch (grails.util.GrailsUtil.environment) {
+          case "development":
+            def facebookId = "development_user"
+            def user = loadUser(facebookId)
+            if(!user){
+              user = new User(name: "Name", facebookId: facebookId)
+              persistenceManager.makePersistent(user)
             }
-        }
-        log.debug("User: ${session.user}")
-        log.debug("Universities: ${session.user.universities}")
-        def universities = session.user?.universities?.collect{
-            if(it != null){
-                log.debug("It is: $it - ${it.class}")
-                persistenceManager.getObjectById(University.class, it)
+            session.user = user
+            break
+
+          case "production":
+            def facebook = facebookService.getFacebookConnection(request, response)
+
+            def facebookUserId = facebook.users_getLoggedInUser();
+            def username = facebook.users_getInfo([facebookUserId], ["name"]).get(0).name
+
+            def user = loadUser(facebookUserId)
+            if (!user) {
+              session.user = new User(facebookId: facebookUserId, name: username)
+              log.debug("Persisting user ${session.user}")
+              persistenceManager.makePersistent(session.user)
+            } else {
+              session.user = new User(facebookId: user.facebookId, name: user.name, id: user.id)
             }
+
+            log.debug("User: ${session.user.name}")
+            break
         }
-        [universities:universities]
+      } catch (e) {
+        log.debug(e)
+      }
     }
+  }
+
+  private def loadUser(facebookUserId) {
+    def query = persistenceManager.newQuery(User.class);
+    query.setFilter("facebookId == facebookIdParam");
+    query.declareParameters("String facebookIdParam");
+
+    query.execute(facebookUserId.toString())
+  }
 }
