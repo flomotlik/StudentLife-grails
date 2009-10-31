35f97fd40edc00f0061024d092e4fd7bd5419bf8
Florian Motlik <florian@florian-laptop.(none)>
2009-10-31 02:27:30 +0100

    Showing colleagues of a Course
    colleagues method of Course controller now shows all keys of the users, which make a specified course.
    
    Signed-off-by: Florian Motlik <florian@florian-laptop.(none)>

--- grails-app/controllers/org/lecturious/CourseController.groovy
+++ grails-app/controllers/org/lecturious/CourseController.groovy
@@ -40,6 +40,20 @@ class CourseController {
     render course.id
   }
 
+  def colleagues = {
+    def key = keyService.courseKey(params.country, params.state, params.city, params.university, params.course)
+    def query = persistenceManager.newQuery(Inscription.class)
+    query.setFilter("course == courseParam")
+    query.declareParameters("com.google.appengine.api.datastore.Key courseParam")
+    def inscriptions = query.execute(key)
+    log.debug("Inscriptions: $inscriptions")
+    def users = []
+    inscriptions.each {
+      users<< it.id.parent
+    }
+    render users
+  }
+
   private def universityKey() {
     persistenceManager.getObjectById(University.class, keyService.universityKey(params.country, params.state,
             params.city, params.university))
