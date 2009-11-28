6c7a07f115a41fa87e2e18c8ec8bc024533b3a5a
Florian Motlik <flomotlik@gmail.com>
2009-11-28 18:29:24 +0100

    Test for UserController and slight bug fixes
    Signed-off-by: Florian Motlik <flomotlik@gmail.com>

--- grails-app/domain/org/lecturious/Inscription.groovy
+++ grails-app/domain/org/lecturious/Inscription.groovy
@@ -5,11 +5,16 @@ import com.google.appengine.api.datastore.Key;
 
 @PersistenceCapable (identityType = IdentityType.APPLICATION, detachable = "true")
 public class Inscription {
-
+  
   @PrimaryKey
   @Persistent (valueStrategy = IdGeneratorStrategy.IDENTITY)
   Key id
-
+  
   @Persistent
   Key course
+  
+  boolean equals(toCompare){
+    log.debug("Equals: $toCompare.course $course")
+    this.course == toCompare.course 
+  }
 }
